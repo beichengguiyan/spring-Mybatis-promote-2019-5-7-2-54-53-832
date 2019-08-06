309ca437b2ac6b30a25059c1412a669dc2d96cbf
yu shuang <763104106@qq.com>
2019-08-06 15:51:39 +0800

    ys

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,9 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.UserMapper;
 import com.example.demo.entity.User;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -14,24 +17,31 @@ public class UserController {
     private UserMapper userMapper;
 
     @GetMapping("/user")
-    public List<User> findAll() {
-        return null;
+    public List<User> findAll(@PathParam("pageNum") int pageNum,@PathParam("pageSize") int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<User> users = userMapper.findAllPage();
+        return users;
+
     }
 
     @GetMapping("/user/{userId}")
-    public User findById(@PathVariable long userId) {
-        return null;
+    public User findById(@PathVariable int userId) {
+        return  userMapper.findByID(userId);
     }
 
     @PostMapping("/user")
     public void insertUser(@RequestBody User user) {
+        userMapper.addUser(user);
     }
 
     @PutMapping("/user/{userId}")
     public void updateUser(@RequestBody User user,@PathVariable int userId) {
+        userMapper.updateUser(user,userId);
     }
 
     @DeleteMapping("/user/{userId}")
-    public void deleteById(@PathVariable long userId) {
+    public void deleteById(@PathVariable int userId) {
+        userMapper.deleteUser(userId);
     }
 }
+
